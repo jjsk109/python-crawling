8353e37ac9ff009e329fb9a5cc6167a48a3ec811
장익순(NickSoon) <39641848+jjsk109@users.noreply.github.com>
2022-11-18 13:34:20 +0900

    라이브 쇼핑사 추가

--- app.py
+++ app.py
@@ -30,9 +30,16 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
 import time
+from datetime import datetime
+
+today = datetime.today().strftime("%Y%m%d")
+
+#
+#   네이버 라이브 쇼핑
+#
 driver = webdriver.Chrome()
 time.sleep(5)
-URL = "https://shoppinglive.naver.com/calendar?d=20221115"
+URL = f"https://shoppinglive.naver.com/calendar?d={today}"
 driver.get(url=URL)
 time.sleep(3)
 page_hight = 3000
@@ -45,34 +52,81 @@ for index in range(1,15):
 html = driver.page_source
 soup = BeautifulSoup(html , "html.parser")
 lis = soup.select(".LiveList_item_3s2bf")
+num = 0
 for li in lis:
     # 시간
     live_time = li.select_one(".LiveItem_time_2l54m")
-    print()
-
+    
     # 시청자 수
-
     on_Air_badge_count = li.select_one(".LiveItem_viewer_n7Q3p")
     LiveItem_viewer_n7Q3p = ""
     if on_Air_badge_count == None :
-        print('방송대기')
-        LiveItem_viewer_n7Q3p = ""
+        LiveItem_viewer_n7Q3p = "방송대기"
     else :
-        print(on_Air_badge_count.text)
         LiveItem_viewer_n7Q3p = on_Air_badge_count.text
 
     # 방송이름
     LiveContent_title = li.select_one(".LiveContent_title_25rkS")
-    print(LiveContent_title.text)
+
     # 상품이름
     LiveContent_product_name_1EjEV = li.select_one(".LiveContent_product_name_1EjEV ")
-    print(LiveContent_product_name_1EjEV.text)
+
     # 가격
     LiveContent_price_2YTb7 = li.select_one(".LiveContent_price_2YTb7 > strong")
-    print(LiveContent_price_2YTb7.text)
+
     # 판매자 이름
     LiveContent_seller_X20Vo = li.select_one(".LiveContent_seller_X20Vo") 
-    print(LiveContent_seller_X20Vo.text)
+    print(f"시간 : {live_time.text} , 판매자 이름 : {LiveContent_seller_X20Vo.text} , 시청자 수 : {LiveItem_viewer_n7Q3p} , 방송이름 : {LiveContent_title.text} , 상품이름 : {LiveContent_product_name_1EjEV.text} , 가격 : {LiveContent_price_2YTb7.text}  ")
 
-    print()
 
+#
+#   카카오 라이브 쇼핑
+#
+
+print()
+time.sleep(1)
+URL = f"https://store.kakao.com/home/live/calendar?date={today}"
+driver.get(url=URL)
+time.sleep(5)
+kakao_html = driver.page_source
+soup = BeautifulSoup(kakao_html , "html.parser")
+kakao_lis = soup.select(".has_alarm")
+num = 0
+
+for li in kakao_lis:
+    txt_time = li.select_one(".txt_time")
+    tit_live = li.select_one(".tit_live")
+    info_play_count = ""
+    info_play = li.select_one(".info_play")
+    if info_play == None:
+        info_play_count = "방송대기"
+    else :
+        info_play_count = info_play.text
+    print(f"시간 : {txt_time.text} , 시청자 수 : {tit_live.text} , 방송이름 : {info_play_count} ")
+print()
+
+#
+#   티몬 라이브 쇼핑
+#
+
+time.sleep(1)
+URL = "http://media.tmon.co.kr/schedule"
+driver.get(url=URL)
+time.sleep(10)
+timon_html = driver.page_source
+soup = BeautifulSoup(timon_html , "html.parser")
+timon_lis = soup.select(".sc-jhAzac.fyAMLI.className")
+for li in timon_lis:
+     txt_time = li.select_one(".sc-brqgnP.ghrAtA")
+     print(txt_time.text)
+
+
+#
+#   그립 라이브 쇼핑
+#
+
+print()
+time.sleep(1)
+URL = f"https://www.grip.show/lives?tab=4"
+driver.get(url=URL)
+time.sleep(5)
